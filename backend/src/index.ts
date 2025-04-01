5185408e770651845932ed2bcdd5336a2a89edec
Arpan Dhama <arpandhama2001@gmail.com>
2025-04-02 00:50:39 +0530

    added routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response, Express } from "express";
+import express, { Express } from "express";
+import { PORT } from "./config/server.config";
+import apiRouter from "./routes";
 
 const app: Express = express();
 
-app.get("/ping", (_req: Request, res: Response) => {
-  res.json({ message: "pong" });
-});
+app.use("/api", apiRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
