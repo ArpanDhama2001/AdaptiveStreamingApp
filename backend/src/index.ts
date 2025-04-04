7f056edf4fcb15211414339a9f0251e6dedf8c4d
Arpan Dhama <arpandhama2001@gmail.com>
2025-04-04 18:14:12 +0530

    added video playback from hls

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,28 @@ import express, { Express } from "express";
 import { PORT } from "./config/server.config";
 import apiRouter from "./routes";
 import cors from "cors";
+import path from "path";
 
 const app: Express = express();
 
 app.use(cors());
 app.use("/api", apiRouter);
 
+app.use('/output', express.static(path.join(__dirname, '../output')));
+
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
+
+// data
+// : 
+// "output/1743770020316/master.m3u8"
+// message
+// : 
+// "Video uploaded successfully"
+// success
+// : 
+// true
+// [[Prototype]]
+// : 
+// Object
